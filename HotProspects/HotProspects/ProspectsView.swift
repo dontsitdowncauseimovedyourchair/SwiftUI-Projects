959c4b01b85c788b97735565db722156113726c0
AleeeGC <183964055+AleeeGC@users.noreply.github.com>
2026-06-19 12:35:43 -0600

    Scanning QR codes!!

--- HotProspects/HotProspects/ProspectsView.swift
+++ HotProspects/HotProspects/ProspectsView.swift
@@ -5,8 +5,10 @@
 //  Created by Alejandro González on 17/06/26.
 //
 
+import CodeScanner
 import SwiftData
 import SwiftUI
+import AVFoundation
 
 struct ProspectsView: View {
     enum FilterType {
@@ -15,6 +17,8 @@ struct ProspectsView: View {
     
     @Environment(\.modelContext) var modelContext
     @Query(sort: \Prospect.name) var prospects: [Prospect]
+    
+    @State private var isShowingScanner = false
     
     let filter: FilterType
     
@@ -45,9 +49,11 @@ struct ProspectsView: View {
             .navigationTitle(title)
             .toolbar {
                 Button("Scan", systemImage: "qrcode.viewfinder") {
-                    let prospect = Prospect(name: "Flop", emailAddress: "flop@flipflop.com", isContacted: false)
-                    modelContext.insert(prospect)
+                    isShowingScanner = true
                 }
+            }
+            .sheet(isPresented: $isShowingScanner) {
+                CodeScannerView(codeTypes: [.qr], simulatedData: "FlipFlop\nflipflop@flop.com", completion: handleScan)
             }
         }
     }
@@ -61,6 +67,22 @@ struct ProspectsView: View {
             _prospects = Query(filter: #Predicate { prospect in
                 prospect.isContacted == showContactedOnly
             }, sort: [SortDescriptor(\Prospect.name)])
+        }
+    }
+    
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        isShowingScanner = false
+        
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+            
+            let person = Prospect(name: details[0], emailAddress: details[1], isContacted: false)
+            
+            modelContext.insert(person)
+        case .failure(let failure):
+            print("Scanning flopped: \(failure.localizedDescription)")
         }
     }
 }
